7c938d5ce11759c5b9542792dd55d73ae296856d
Pradeep Raj Thapaliya <pradeep.thapaliya95@gmail.com>
2020-04-09 17:07:37 -0500

    shared proto added

---
+++ Client.SharedProto/Program.cs
@@ -0,0 +1,33 @@
+﻿using gRPC.Core.Server;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Client.SharedProto
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var message = "";
+            Console.WriteLine("Type your name: ");
+            message = Console.ReadLine();
+
+            while (true)
+            {
+                var reply = await client.SayHelloAsync(
+                             new HelloRequest { Name = $"{message}" });
+                Console.WriteLine("Greeting: " + reply.Message);
+                Console.WriteLine("Press 'exit' to exit... or type your name : ");
+                message = Console.ReadLine();
+                if (message.ToLower() == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
